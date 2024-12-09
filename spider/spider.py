74658ebbbba3abf62c462f32ab922e92ecf2bbe2
Cvandia <106718176+Cvandia@users.noreply.github.com>
2024-12-09 20:54:44 +0800

    ✨ 更新 spider-workflow.yml，合并依赖安装与爬虫运行步骤；在 requirements.txt 中添加 loguru 库；在 spider.py 中使用 loguru 替代 print 进行日志记录

--- spider/spider.py
+++ spider/spider.py
@@ -6,6 +6,7 @@ import random
 from bs4 import BeautifulSoup
 from datetime import datetime
 from googletrans import Translator
+from loguru import logger
 
 site_url = "https://fitgirl-repacks.site"
 
@@ -21,7 +22,7 @@ for link in page_links:
         page_number = int(page_text)
         end_page = max(end_page, page_number)
 if end_page < start_page:
-    print("\n未获取到有效页码，程序中止")
+    logger.error("未获取到有效页码，程序中止")
     os._exit(0)
 
 # 爬取数据
@@ -29,7 +30,7 @@ data_list = []
 failed_attempts = 0
 while start_page <= end_page:
     # 开始爬取数据
-    print(f"\n正在爬取第 {start_page}/{end_page} 页")
+    logger.info(f"正在爬取第 {start_page}/{end_page} 页")
     url = f"{site_url}/page/{start_page}"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -39,9 +40,9 @@ while start_page <= end_page:
     if not articles:
         failed_attempts += 1
         if failed_attempts > 3:
-            print("\n已连续失败 3 次，程序中止")
+            logger.error("已连续失败 3 次，程序中止")
             os._exit(0)
-        print("\n爬取数据失败，暂停 5 分钟后重试")
+        logger.warning("爬取数据失败，暂停 5 分钟后重试")
         time.sleep(300)
         continue
 
@@ -59,7 +60,7 @@ while start_page <= end_page:
         article_content_element = article.select_one("div.su-spoiler-title:-soup-contains('Game Description') + div.su-spoiler-content")
         if article_link_element:
             article_id = article.get("id").split("-")[-1]
-            print(f"√ 已保存第 {now_article}/{len(articles)} 条数据")
+            logger.info(f"√ 已保存第 {now_article}/{len(articles)} 条数据")
             article_title = article_title_element.text.strip() if article_title_element else None
             article_time = article_time_element.get("datetime") if article_time_element else None
             article_link = article_link_element.get("href")
@@ -74,15 +75,15 @@ while start_page <= end_page:
                 if article_content:
                     article_content = translator.translate(article_content, src='en', dest='zh-cn').text
             except Exception as e:
-                print(f"× 翻译失败：{e}")
+                logger.error(f"× 翻译失败：{e}")
             data_list.append([article_id, article_title, article_time, article_link, article_cover, article_description, article_content])
         else:
-            print(f"× 抛弃第 {now_article}/{len(articles)} 条数据")
+            logger.warning(f"× 抛弃第 {now_article}/{len(articles)} 条数据")
 
     # 每循环3次随机暂停
     if failed_attempts == 0 and start_page % 3 == 0:
         pause_time = random.randint(3, 5)
-        print(f"\n暂停 {pause_time} 秒")
+        logger.info(f"暂停 {pause_time} 秒")
         time.sleep(pause_time)
 
     # 重置失败次数计数器
@@ -98,7 +99,7 @@ if os.path.exists(config_file):
         previous_count = int(f.read())
 else:
     previous_count = 0
-print(f"\n共获取到 {len(data_list)} 条有效数据，上次 {previous_count} 条")
+logger.info(f"共获取到 {len(data_list)} 条有效数据，上次 {previous_count} 条")
 
 # 检查是否满足写入条件
 if len(data_list) >= previous_count:
@@ -111,12 +112,12 @@ if len(data_list) >= previous_count:
         writer = csv.writer(csvfile)
         writer.writerow(["ID", "标题", "时间", "链接", "封面", "说明", "简介"])
         writer.writerows(data_list)
-    print(f"\n数据文件 {csv_file} 已更新")
+    logger.info(f"数据文件 {csv_file} 已更新")
 
     # 更新配置文件
     with open(config_file, "w") as f:
         f.write(str(len(data_list)))
-    print(f"配置文件 {config_file} 已更新")
+    logger.info(f"配置文件 {config_file} 已更新")
 
     # 更新 README 文件
     template_file = "./readme.txt"
@@ -129,7 +130,7 @@ if len(data_list) >= previous_count:
         template_content = template_content.replace("{{articletitle}}", data_list[i][1], 1)
     with open(md_file, "w", encoding="utf-8") as f:
         f.write(template_content)
-    print(f"README 文件 {md_file} 已更新")
+    logger.info(f"README 文件 {md_file} 已更新")
 
     # 更新 HTML 模板
     template_file = "./template.txt"
@@ -139,6 +140,6 @@ if len(data_list) >= previous_count:
     template_content = template_content.replace("{{lastupdated}}", current_time)
     with open(html_file, "w", encoding="utf-8") as f:
         f.write(template_content)
-    print(f"HTML 模板 {html_file} 已更新")
+    logger.info(f"HTML 模板 {html_file} 已更新")
 else:
-    print("\n爬取内容不完整，放弃数据更新")
+    logger.warning("爬取内容不完整，放弃数据更新")
